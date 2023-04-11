f91b5ea46c367dea75e32b5c092687f25f6a4a82
Corentin Braud <corentin.braud95@gmail.com>
2023-04-11 22:48:45 +0200

    Fonctionnalité connexion ajouté et modification form

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,44 +1,34 @@
-const config = {
-    "userId": 1,
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
-}
-const authKey = config.token;
+const formulaireLogin = document.querySelector("form");
 
-function Login() {
-    const formulaireLogin = document.querySelector("form");
-    formulaireLogin.addEventListener("submit", function (event) {
-        event.preventDefault();
-        // Création de l’objet avec le mail et password
-        let email = event.target.querySelector("[name=email]").value;
-        let password = event.target.querySelector("[name=password]").value;
-        const log = {
-            email: email,
-            password: password
-        };
-        // Création de la charge utile au format JSON
-        const chargeUtile = JSON.stringify(log);
-        // Appel de la fonction fetch avec toutes les informations nécessaires
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': `Bearer ${authKey}`
-            },
-            body: chargeUtile
+formulaireLogin.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    // Création de l’objet avec le mail et password
+    const user = {
+        email: event.target.querySelector("[name=email]").value,
+        password: event.target.querySelector("[name=password]").value
+    };
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            if (response.ok) {
+                // La requête a réussi, redirection vers la page d'accueil
+                window.location.href = 'index.html';
+            } else {
+                // La requête a échoué, affichage d'un message d'erreur
+                throw new Error('Identifiants incorrects');
+            }
         })
-            .then(response => {
-                if (response.ok) {
-                    // La requête a réussi, redirection vers la page d'accueil
-                    window.location.href = 'index.html';
-                } else {
-                    // La requête a échoué, affichage d'un message d'erreur
-                    throw new Error('Identifiants incorrects');
-                }
-            })
-            .catch(error => {
-                console.error(error);
-                // Affichage d'un message d'erreur
-                alert('Une erreur est survenue lors de la connexion');
-            });
-    });
-}
+        .catch(error => {
+            console.error(error);
+            // Affichage d'un message d'erreur
+            alert('Identifiants incorrects');
+        });
+});
+
+
+formulaireLogin.addEventListener("submit", logSubmit);
+
