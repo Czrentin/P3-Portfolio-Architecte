0973ffc0dc7f64db81def335812af78f29c9d2fe
Corentin Braud <corentin.braud95@gmail.com>
2023-04-17 23:21:03 +0200

    Amélioration scss, changement nom des class, uniformisation

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,37 +3,59 @@ projets = JSON.parse(projets)
 
 async function chargerAPI() {
     try {
-        const reponse = await fetch('http://localhost:5678/api/works');
-        const projets = await reponse.json();
-        const valeurProjets = JSON.stringify(projets);
+        const reponse = await fetch('http://localhost:5678/api/works')
+        const projets = await reponse.json()
+        const valeurProjets = JSON.stringify(projets)
         // Stockage des informations dans le localStorage
-        window.localStorage.setItem("projets", valeurProjets);
+        window.localStorage.setItem("projets", valeurProjets)
         // Appeler la fonction pour générer les projets avec les données récupérées
         genererProjets(projets)
         genererGallery(projets)
     } catch (error) {
-        console.error("Une erreur est survenue lors de la récupération des données des projets :", error);
+        console.error("Une erreur est survenue lors de la récupération des données des projets :", error)
     }
 }
 
 function genererProjets(projets) {
     for (let i = 0; i < projets.length; i++) {
 
-        const article = projets[i];
         // Récupération de l'élément du DOM qui accueillera les projets
-        const sectionProjets = document.querySelector(".gallery");
+        const sectionProjets = document.querySelector(".gallery")
         // Création d’une balise dédiée pour un projet
-        const projetElement = document.createElement("figure");
+        const projetElement = document.createElement("figure")
         projetElement.dataset.id = projets[i].id
         // Création des balises à l'intérieur
-        const imageElement = document.createElement("img");
-        imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerText = article.title;
+        const imageElement = document.createElement("img")
+        imageElement.src = projets[i].imageUrl
+        const titleElement = document.createElement("figcaption")
+        titleElement.innerText = projets[i].title
         // On rattache la balise article a la section Projets
-        projetElement.appendChild(imageElement);
-        projetElement.appendChild(titleElement);
-        sectionProjets.appendChild(projetElement);
+        projetElement.appendChild(imageElement)
+        projetElement.appendChild(titleElement)
+        sectionProjets.appendChild(projetElement)
+    }
+}
+
+function genererGallery(projets) {
+    for (let i = 0; i < projets.length; i++) {
+
+        // Récupération de l'élément du DOM qui accueillera les projets
+        const sectionProjets = document.querySelector('.gallery-modal')
+        // Création d’une balise dédiée pour un projet
+        const projetElement = document.createElement('figure')
+        projetElement.dataset.id = projets[i].id
+        // Création des balises à l'intérieur
+        const imageElement = document.createElement('img')
+        imageElement.src = projets[i].imageUrl
+        const iconElement = document.createElement('i')
+        iconElement.classList.add('fa-solid', 'fa-trash-can')
+        const titleElement = document.createElement('figcaption')
+        titleElement.innerText = 'éditer'
+        // On rattache la balise article a la section Projets
+        projetElement.appendChild(imageElement)
+        projetElement.appendChild(iconElement)
+        projetElement.appendChild(titleElement)
+        sectionProjets.appendChild(projetElement)
     }
 }
 
@@ -42,12 +64,13 @@ chargerAPI()
 //
 // Partie pour le filtre
 //
+const filtres = document.querySelector('.filtre')
+const buttonsFiltres = filtres.querySelectorAll('button')
 
-const boutonsTri = document.querySelectorAll(".btn-tri")
 // Fonction qui tri en fonction du data-category 
-boutonsTri.forEach(function (boutonTri) {
-    boutonTri.addEventListener("click", function () {
-        const categoryId = parseInt(boutonTri.dataset.category)
+buttonsFiltres.forEach(function (buttonsFiltres) {
+    buttonsFiltres.addEventListener("click", function () {
+        const categoryId = parseInt(buttonsFiltres.dataset.category)
         let projetsFiltres
         if (categoryId === 0) {
             projetsFiltres = projets
@@ -62,47 +85,47 @@ boutonsTri.forEach(function (boutonTri) {
 })
 
 // Permet d'afficher le bouton du filtre sélectionné en "actif" en changeant sa couleur etc
-const filtres = document.querySelector('.filtre')
-const buttonsFiltres = filtres.querySelectorAll('button')
-
 for (let i = 0; i < buttonsFiltres.length; i++) {
     buttonsFiltres[i].addEventListener("click", function () {
-        let active = document.getElementsByClassName("active")
+        const filtreContainer = document.querySelector('.filtre')
+        let green = filtreContainer.getElementsByClassName('btn-green')
 
-        // si pas de class active
-        if (active.length > 0) {
-            active[0].className = active[0].className.replace(" active", "")
+        // si pas de class btn-green
+        if (green.length > 0) {
+            green[0].className = green[0].className.replace(" btn-green", "")
         }
-        // Ajoute la class active au btn cliqué
-        this.className += " active"
-    });
+        // Ajoute la class btn-green au btn cliqué
+        this.className += " btn-green"
+    })
 }
 
 //
 // Partie connectée
 //
 
-// Masque le filtre
-function hideFiltre() {
+// vérifie si la paire clé-valeur dans le sessionStorage
+let connected = sessionStorage.getItem("token") !== null
+
+if (connected) {
+    // Masque le filtre
     const filtres = document.querySelector('.filtre')
     filtres.style.display = "none"
-}
-// Change le login en logout
-function logout() {
-    // Récupérez l'élément <a> dans la liste
+
+    // Change le login en logout
     const a = document.querySelector('nav ul li:nth-child(3) a')
     a.innerText = 'logout'
-}
-// Affiche les boutons modifier pour afficher la modale
-function modifierButton() {
-    const divButton = Array.from(document.querySelectorAll('.div-modifier'))
+    // Supprime le token quand in appui sur logout
+    a.addEventListener("click", function () {
+        window.sessionStorage.removeItem("token");
+    })
+
+    // Afficher les boutons modifier pour la modale
+    const divButton = Array.from(document.querySelectorAll('.div-button-modifier'))
     divButton.forEach(divButton => {
         divButton.style.display = "flex"
-    });
-}
+    })
 
-// Ajout barre au mode édition 
-function barreModifier() {
+    // Ajout barre mode édition
     const bodyElement = document.querySelector('body')
 
     const barreModifier = document.createElement('div')
@@ -114,6 +137,7 @@ function barreModifier() {
     p.innerText = 'Mode édition'
 
     const publish = document.createElement('button')
+    publish.classList.add('btn-white')
     publish.innerText = 'publier les changements'
 
     bodyElement.parentNode.insertBefore(barreModifier, bodyElement)
@@ -122,103 +146,9 @@ function barreModifier() {
     barreModifier.appendChild(publish)
 }
 
-let connected = sessionStorage.getItem("token") !== null
-
-if (connected) { // vérifie si la paire clé-valeur dans le sessionStorage
-    const logout = document.querySelector('nav ul li:nth-child(3) a')
-    logout.addEventListener("click", function () {
-        window.sessionStorage.removeItem("token");
-    })
-}
-
-if (connected) { // vérifie si la paire clé-valeur dans le sessionStorage
-    hideFiltre()
-    logout()
-    modifierButton()
-    barreModifier()
-}
-
 //
 // Partie pour la gestion de la modale
 //
-
-// Créer l'élément <aside>
-const aside = document.createElement("aside");
-aside.id = "modal";
-aside.classList.add("modal");
-aside.setAttribute("aria-hidden", "true");
-aside.setAttribute("role", "dialog");
-aside.setAttribute("aria-labelledby", "titlemodal");
-
-// Créer la structure de l'élément <aside>
-const modalWrapper = document.createElement("div");
-modalWrapper.classList.add("modal-wrapper");
-
-const divCloseModal = document.createElement("div");
-divCloseModal.classList.add("div-close-modal");
-
-const buttonCloseModal = document.createElement("button");
-buttonCloseModal.classList.add("close-modal");
-
-const spanCloseModal = document.createElement("span");
-spanCloseModal.textContent = "Fermer la boite modale";
-
-const iconCloseModal = document.createElement("i");
-iconCloseModal.classList.add("fa-solid", "fa-x");
-
-buttonCloseModal.appendChild(spanCloseModal);
-buttonCloseModal.appendChild(iconCloseModal);
-divCloseModal.appendChild(buttonCloseModal);
-
-const titleModal = document.createElement("h2");
-titleModal.id = "titlemodal";
-titleModal.textContent = "Galerie photo";
-
-const galleryModal = document.createElement("div");
-galleryModal.classList.add("gallery-modal");
-
-const hr = document.createElement("hr");
-
-const buttonAddPicture = document.createElement("button");
-buttonAddPicture.classList.add("add-picture");
-buttonAddPicture.textContent = "Ajouter une photo";
-
-const pDeleteGallery = document.createElement("p");
-pDeleteGallery.textContent = "Supprimer la galerie";
-
-modalWrapper.appendChild(divCloseModal);
-modalWrapper.appendChild(titleModal);
-modalWrapper.appendChild(galleryModal);
-modalWrapper.appendChild(hr);
-modalWrapper.appendChild(buttonAddPicture);
-modalWrapper.appendChild(pDeleteGallery);
-
-aside.appendChild(modalWrapper);
-
-// Insérer l'élément <aside> après la section avec l'ID "introduction"
-const introductionSection = document.getElementById("introduction");
-introductionSection.parentNode.insertBefore(aside, introductionSection.nextSibling);
-
-function genererGallery(projets) {
-    for (let i = 0; i < projets.length; i++) {
-
-        const article = projets[i];
-        // Récupération de l'élément du DOM qui accueillera les projets
-        const sectionProjets = document.querySelector(".gallery-modal");
-        // Création d’une balise dédiée pour un projet
-        const projetElement = document.createElement("figure");
-        projetElement.dataset.id = projets[i].id
-        // Création des balises à l'intérieur
-        const imageElement = document.createElement("img");
-        imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerText = 'éditer'
-        // On rattache la balise article a la section Projets
-        projetElement.appendChild(imageElement);
-        projetElement.appendChild(titleElement);
-        sectionProjets.appendChild(projetElement);
-    }
-}
 
 let modal = null
 
@@ -237,7 +167,7 @@ const openModal = function (e) {
     modal = target
     modal.addEventListener('click', closeModal)
     modal.querySelector('.close-modal').addEventListener('click', closeModal)
-    modal.querySelector('.modal-wrapper').addEventListener('click', stopPropagation)
+    modal.querySelector('.container-modal').addEventListener('click', stopPropagation)
 }
 
 // Fermeture de la modale
@@ -249,12 +179,12 @@ const closeModal = function (e) {
     modal.removeAttribute('aria-modal')
     modal.removeEventListener('click', closeModal)
     modal.querySelector('.close-modal').removeEventListener('click', closeModal)
-    modal.querySelector('.modal-wrapper').removeEventListener('click', stopPropagation)
+    modal.querySelector('.container-modal').removeEventListener('click', stopPropagation)
     modal = null
 }
 
 // Ouverture de la modale au click
-document.querySelectorAll('.modifier').forEach(a => {
+document.querySelectorAll('.button-modifier').forEach(a => {
     a.addEventListener('click', openModal)
 })
 
